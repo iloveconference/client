7342aa9edc2615d41a4c9944d0cf0eca620c6a46
Dallan Quass <dallan@quass.org>
2023-02-22 09:47:13 -0700

    add health check

---
+++ src/routes/health/+server.ts
@@ -0,0 +1,7 @@
+import { env } from '$env/dynamic/private'
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async () => {
+    const requestUrl = `${env.SERVER_HOST}/health`
+    return await fetch(requestUrl)
+};
